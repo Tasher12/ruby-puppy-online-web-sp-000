5fed1d9cd1a53456708f326bd6abccd530d6d230
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-02-22 16:27:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
 # Add your code here
+
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor = :dog
+  
+  
+  def initialize(dog)
+    @dog = dog
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+def self.clear_all
+    @@all.clear 
+  end
+  
+  def self.print_all
+    @@all.each do |x|
+      puts x.dog 
+    end
+  end
+  
+end
